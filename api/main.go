e9dbfa8bdbbd3f09e5cb412b6f77df405d4b1ad2
watanabeJunna <junna7897@icloud.com>
2019-09-04 19:10:28 +0900

    change indent

--- api/main.go
+++ api/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-  "context"  // Use "golang.org/x/net/context" for Golang version <= 1.6
-  "flag"
-  "net/http"
+    "context"  // Use "golang.org/x/net/context" for Golang version <= 1.6
+    "flag"
+    "net/http"
 
-  "github.com/golang/glog"
-  "github.com/grpc-ecosystem/grpc-gateway/runtime"
-  "google.golang.org/grpc"
+    "github.com/golang/glog"
+    "github.com/grpc-ecosystem/grpc-gateway/runtime"
+    "google.golang.org/grpc"
 
-  gw "authenticationservice/pb"
+    gw "user/pb"
 )
 
 var (
-  grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:9090", "gRPC server endpoint")
+    grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:9090", "gRPC server endpoint")
 )
 
 func run() error {
-  ctx := context.Background()
-  ctx, cancel := context.WithCancel(ctx)
-  defer cancel()
+    ctx := context.Background()
+    ctx, cancel := context.WithCancel(ctx)
+    defer cancel()
 
-  // Register gRPC server endpoint
-  // Note: Make sure the gRPC server is running properly and accessible
-  mux := runtime.NewServeMux()
-  opts := []grpc.DialOption{grpc.WithInsecure()}
-  err := gw.RegisterUserServiceHandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
-  if err != nil {
-    return err
-  }
+    // Register gRPC server endpoint
+    // Note: Make sure the gRPC server is running properly and accessible
+    mux := runtime.NewServeMux()
+    opts := []grpc.DialOption{grpc.WithInsecure()}
+    err := gw.RegisterUserServiceHandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
+    if err != nil {
+        return err
+    }
 
-  return http.ListenAndServe(":8081", mux)
+    return http.ListenAndServe(":8081", mux)
 }
 
 func main() {
-  flag.Parse()
-  defer glog.Flush()
+    flag.Parse()
+    defer glog.Flush()
 
-  if err := run(); err != nil {
-    glog.Fatal(err)
-  }
+    if err := run(); err != nil {
+        glog.Fatal(err)
+    }
 }
