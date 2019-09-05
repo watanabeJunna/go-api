2cb15a896863665931d13173013d9b74263c2287
watanabeJunna <junna7897@icloud.com>
2019-09-05 14:48:54 +0900

    deleted pbbuild.sh

--- pbbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-cd $GOPATH/src/user/
-
-protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. pb/user.proto
-
-protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. pb/user.proto
