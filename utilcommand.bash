bc8de5de6d7c255c6e1e1efff79219ce01f8ebef
watanabeJunna <junna7897@icloud.com>
2019-09-05 14:37:03 +0900

    rename function

--- utilcommand.bash
+++ utilcommand.bash
@@ -1,18 +1,18 @@
 ## $ . utilcommand.sh
 
-upserver() {
+up() {
     for c in "go run $(find -name server)/main.go" "go run $(find -name api)/main.go"; do 
         ${c} &
     done
 }
 
-downserver() {
+down() {
     ps -ef | grep server/main.go | grep -v grep | awk '{print $1}' | xargs -I{} kill -9 {}
     ps -ef | grep server/api.go  | grep -v grep | awk '{print $1}' | xargs -I{} kill -9 {}
     ps -ef | grep go-build | grep -v grep | awk '{print $1}' | xargs -I{} kill -9 {}
 }
 
-pbbuild() {
+buildpb() {
     cd $GOPATH/src/user/
     protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. pb/user.proto
     protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. pb/user.proto
