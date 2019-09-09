9a8d429a9ca6328059f5220ef17e9642c87a9938
watanabeJunna <junna7897@icloud.com>
2019-09-09 16:54:49 +0900

    fix indent

--- lib/cipher/cipher.go
+++ lib/cipher/cipher.go
@@ -14,61 +14,61 @@ func encodeBase64(b []byte) string {
 }
 
 func decodeBase64(s string) []byte {
-	data, err := base64.StdEncoding.DecodeString(s)
-	
+    data, err := base64.StdEncoding.DecodeString(s)
+    
     if err != nil {
         panic(err)
-	}
-	
+    }
+    
     return data
 }
 
 func Encrypt(key string, text string) (string, error) {
-	block, err := aes.NewCipher([]byte(key))
-	
+    block, err := aes.NewCipher([]byte(key))
+    
     if err != nil {
         return "", err
-	}
-	
-	b := base64.StdEncoding.EncodeToString([]byte(text))
-	cipt := make([]byte, aes.BlockSize+len(b))
-	iv := cipt[:aes.BlockSize]
-	
+    }
+    
+    b := base64.StdEncoding.EncodeToString([]byte(text))
+    cipt := make([]byte, aes.BlockSize+len(b))
+    iv := cipt[:aes.BlockSize]
+    
     if _, err := io.ReadFull(rand.Reader, iv); err != nil {
         return "", err
-	}
-	
-	cfb := cipher.NewCFBEncrypter(block, iv)
-	
-	cfb.XORKeyStream(cipt[aes.BlockSize:], []byte(b))
-	
+    }
+    
+    cfb := cipher.NewCFBEncrypter(block, iv)
+    
+    cfb.XORKeyStream(cipt[aes.BlockSize:], []byte(b))
+    
     return encodeBase64(cipt), nil
 }
 
 func Decrypt(key string, t string) (string, error) {
-	block, err := aes.NewCipher([]byte(key))
-	
+    block, err := aes.NewCipher([]byte(key))
+    
     if err != nil {
         return "", err
-	}
-	
-	text := decodeBase64(t)
-	
+    }
+    
+    text := decodeBase64(t)
+    
     if len(text) < aes.BlockSize {
         return "", errors.New("too short")
-	}
-	
+    }
+    
     iv := text[:aes.BlockSize]
     text = text[aes.BlockSize:]
-	cfb := cipher.NewCFBDecrypter(block, iv)
-	
-	cfb.XORKeyStream(text, text)
-	
-	data, err := base64.StdEncoding.DecodeString(string(text))
-	
+    cfb := cipher.NewCFBDecrypter(block, iv)
+    
+    cfb.XORKeyStream(text, text)
+    
+    data, err := base64.StdEncoding.DecodeString(string(text))
+    
     if err != nil {
         return "", err
-	}
-	
+    }
+    
     return string(data), nil
 }
--- lib/cipher/cipher_test.go
+++ lib/cipher/cipher_test.go
@@ -3,22 +3,22 @@ package cipher
 import "testing"
 
 func TestCipher(t *testing.T) {
-	src := "WatanabeJunna"
-	key := "8Fmj4kb4oWpw9g=="
+    src := "WatanabeJunna"
+    key := "8Fmj4kb4oWpw9g=="
 
-	enc, err := Encrypt(key, src)
-	
+    enc, err := Encrypt(key, src)
+    
     if err != nil {
         panic(err)
-	}
+    }
 
-	dec, err := Decrypt(key, enc)
-	
+    dec, err := Decrypt(key, enc)
+    
     if err != nil {
         panic(err)
-	}
-	
-	if dec != src {
-		t.Errorf("src = %q, dec %q", src, dec)
-	}
+    }
+    
+    if dec != src {
+        t.Errorf("src = %q, dec %q", src, dec)
+    }
 }
